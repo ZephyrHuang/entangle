c341de0c6491fa43ae8e004f6b34ae40ece72a79
ZephyrHuang <zephyrhuang21@163.com>
2018-08-28 12:57:59 +0800

    Add cache in clipboard subscriber to avoid echo between endpoints in some cases.

--- src/main/java/me/zephyr/entangle/transport/stomp/stompsessionhandler/SubscribeLatestClipboardContentHandler.java
+++ src/main/java/me/zephyr/entangle/transport/stomp/stompsessionhandler/SubscribeLatestClipboardContentHandler.java
@@ -1,6 +1,7 @@
 package me.zephyr.entangle.transport.stomp.stompsessionhandler;
 
 import me.zephyr.entangle.clipboard.ClipboardOperator;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,13 @@ import org.springframework.stereotype.Component;
 public class SubscribeLatestClipboardContentHandler extends AbstractStompSessionHandler {
   private static final Logger logger = LoggerFactory.getLogger(SubscribeLatestClipboardContentHandler.class);
 
+  private static final Object monitor = new Object();
   @Autowired
   private ClipboardOperator clipboardOperator;
+  /**
+   * 缓存最近接收到的文本，防止被第三方软件影响在两个端点之间产生回响。
+   */
+  private volatile String stringPushedLastTime;
 
   @Override
   public void handleFrame(StompHeaders headers, Object payload) {
@@ -21,6 +27,16 @@ public class SubscribeLatestClipboardContentHandler extends AbstractStompSession
       return;
     }
     String incoming = (String) payload;
-    clipboardOperator.pushIntoSystemClipboard(incoming);
+    if (StringUtils.isBlank(incoming)) {
+      return;
+    }
+    if (!incoming.equals(stringPushedLastTime)) {
+      synchronized (monitor) {
+        if (!incoming.equals(stringPushedLastTime)) {
+          clipboardOperator.pushIntoSystemClipboard(incoming);
+          stringPushedLastTime = incoming;
+        }
+      }
+    }
   }
 }
